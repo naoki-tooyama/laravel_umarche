0fc59ab3c3be2a782382a17733d9832098795380
naoki tooyama <ntooyama.1112@gmail.com>
2024-07-30 11:25:00 +0900

    product create

--- database/migrations/2024_07_29_135601_create_products_table.php
+++ database/migrations/2024_07_29_135601_create_products_table.php
@@ -15,15 +15,15 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('information');//->nullable();
+            $table->unsignedInteger('price');//->nullable();
+            $table->boolean('is_selling');//->nullable();
+            $table->integer('sort_order')->nullable();
             $table->foreignId('shop_id')
                 ->constrained()
                 ->onUpdate('Cascade')
                 ->onDelete('Cascade');
-            $table->string('name');
-            $table->text('information')->nullable();
-            $table->unsignedInteger('price')->nullable();
-            $table->boolean('is_selling')->nullable();
-            $table->integer('sort_order')->nullable();
             $table->foreignId('secondary_category_id')
                     ->constrained();
             $table->foreignId('image1')
