72ace71daaf89a788c423fb43984b323bac81bf8
naoki tooyama <ntooyama.1112@gmail.com>
2024-07-31 16:39:37 +0900

    cart add

---
+++ app/Http/Controllers/User/CartController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use App\Models\Cart;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class CartController extends Controller
+{
+    public function add(Request $request){
+        // dd($request);
+        $itemInCart = Cart::where('user_id', Auth::id())
+            ->where('product_id', $request->product_id)->first();//カートに商品があるか確認
+
+        if($itemInCart){
+            $itemInCart->quantity += $request->quantity;//同じ商品があれば数量追加
+            $itemInCart->save();
+        }else{
+            Cart::create([
+                'user_id' => Auth::id(),
+                'product_id' => $request->product_id,
+                'quantity' => $request->quantity,
+            ]);
+        }
+
+        return redirect()->route('user.cart.index');
+    }
+}
