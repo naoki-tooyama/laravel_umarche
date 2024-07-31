c1fe013d25a1f70b17e75c1ec1bdf7d6848be93c
naoki tooyama <ntooyama.1112@gmail.com>
2024-07-31 15:00:47 +0900

    swiper

--- public/js/app.js
+++ public/js/app.js
@@ -5464,6 +5464,8 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var alpinejs__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! alpinejs */ "./node_modules/alpinejs/dist/module.esm.js");
 
 
+// import './swiper';
+
 window.Alpine = alpinejs__WEBPACK_IMPORTED_MODULE_1__["default"];
 alpinejs__WEBPACK_IMPORTED_MODULE_1__["default"].start();
 
