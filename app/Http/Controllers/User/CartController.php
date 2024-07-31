e696a2c9ebe032aaf1377e3efdbc56a4e4e7ebea
naoki tooyama <ntooyama.1112@gmail.com>
2024-07-31 17:58:10 +0900

    stripe controller

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -52,4 +52,39 @@ class CartController extends Controller
             ->with(['message' => '商品を削除しました。',
             'status' => 'alert']);
     }
+
+    public function checkout(){
+        $user = User::findOrFail(Auth::id());
+        $products = $user->products;
+        
+        
+        $lineItems = [];
+        foreach($user->products as $product){
+            $line_item =[
+                'name' => $product->name,
+                'description' => $product->description,
+                'amount' => $product->price,
+                'currency' => 'jpy',
+                'quantity' => $product->pivot->quantity,
+            ];
+            array_push($lineItems, $line_item);
+        }
+// dd($lineItems, $line_item);
+        // dd($products, $totalPrice);
+
+        \Stripe\Stripe::setApiKey(env('STRIPE_SECRET_KEY'));
+
+        $session = \Stripe\Checkout\Session::create([
+            'payment_method_types' => ['card'],
+            'line_items' => [$lineItems],
+            'mode' => 'payment',
+            'success_url' => route('user.cart.success'),
+            'cancel_url' => route('user.cart.cancel'),
+        ]);
+
+        $publicKey = env('STRIPE_PUBLIC_KEY');
+
+        return view('user.checkout', 
+            compact('session', 'publicKey'));
+    }
 }
